1b2e2a161ed3c537e775d1a1e79a04a31195b001
Papy87 <papucic87@gmail.com git config --global user.name Papy87  git config --global user.email papucic87@gmail.com>
2019-06-03 13:04:11 +0200

    Commit

--- bilo sta/src/app/shared/mainFetchUrl.js
+++
@@ -1,4 +0,0 @@
-const BASE_URL = 'https://book-api.hypetech.xyz/v1'
-
-
-export default BASE_URL
--- bilo sta/src/app/shared/mainVaribales.js
+++
@@ -1,9 +0,0 @@
-const BASE_URL = 'https://book-api.hypetech.xyz/v1';
-const API_KEY = 'B1tPr0d';
-const ADMIN_ID = 11;
-
-export { BASE_URL, API_KEY, ADMIN_ID }
-
-
-// const BASE_URL = 'http://book-api-dev.hypetech.xyz'
-// "x-api-key": "B1tPr0d"
